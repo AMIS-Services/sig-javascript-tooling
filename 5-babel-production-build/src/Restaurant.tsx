e23fa6c0071e08c28c17203d0dc9fd79592e0f07
Kjettil Hennis <kjettil.hennis@amis.nl>
2019-08-07 20:35:19 +0200

    Start with Step 5

---
+++ 5-babel-production-build/src/Restaurant.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+interface IProps {
+  name: string;
+  city: string;
+  stars: number;
+}
+
+const Restaurant = (props: IProps) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.city}</h3>
+      <h4>{props.stars}</h4>
+    </div>
+  )
+}
+
+export default Restaurant
